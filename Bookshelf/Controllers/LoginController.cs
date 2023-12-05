c832e50eebbfa4e669e644c3a5250abd2ec13fcd
Raj Mehta A00475804 <raj.mehta@smu.ca>
2023-12-05 01:30:08 -0400

    added login and registeration apis

--- Bookshelf/Controllers/LoginController.cs
+++ Bookshelf/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Bookshelf.Models;
+﻿using Bookshelf.Data;
+using Bookshelf.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,14 @@ namespace Bookshelf.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+
+        private readonly BookshelfDbContext _context;
+
+        public LoginController(BookshelfDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpPost]
         public IActionResult Authenticate([FromBody] LoginRequest model)
         {
@@ -31,5 +40,83 @@ namespace Bookshelf.Controllers
             // Check if the provided credentials match the static ones
             return username == validUsername && password == validPassword;
         }
+
+        //code for register and login
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] UserRegisterRequest userRegisterRequest)
+        {
+            try
+            {
+                // Validate the request
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                // Check if the username or email already exists
+                if (_context.Users.Any(u => u.Username == userRegisterRequest.Username))
+                {
+                    return BadRequest("Username already exists.");
+                }
+
+                if (_context.Users.Any(u => u.Email == userRegisterRequest.Email))
+                {
+                    return BadRequest("Email address already exists.");
+                }
+
+                // Create a new user
+                var newUser = new User
+                {
+                    Name = userRegisterRequest.Name,
+                    Username = userRegisterRequest.Username,
+                    Password = userRegisterRequest.Password, // You should hash the password in a real-world scenario
+                    Email = userRegisterRequest.Email
+                    // Add other properties as needed
+                };
+
+                // Add the user to the database
+                _context.Users.Add(newUser);
+                _context.SaveChanges();
+
+                return Ok("Registration successful.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest loginRequest)
+        {
+            try
+            {
+                // Validate the request
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                // Find the user by username
+                var user = _context.Users
+                    .SingleOrDefault(u => u.Username == loginRequest.Username);
+
+                // Check if the user exists and verify the password
+                if (user == null || user.Password != loginRequest.Password)
+                {
+                    return BadRequest("Invalid username or password.");
+                }
+
+                // You may want to implement token-based authentication here in a production scenario
+                // For simplicity, let's just return a success message in this example
+
+                return Ok($"Login successful for user: {user.Username}");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+            }
+        }
+
     }
 }
