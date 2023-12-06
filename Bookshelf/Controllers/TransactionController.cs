ce235e784413720f139b75d226ad01bf110d4b0d
A00472830 <princeton.hilary.dcunha@smu.ca>
2023-12-06 15:51:35 -0400

    Setup Order APIs

---
+++ Bookshelf/Controllers/TransactionController.cs
@@ -0,0 +1,43 @@
+﻿using Bookshelf.Data;
+using Bookshelf.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookshelf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly BookshelfDbContext _context;
+
+        public TransactionController(BookshelfDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AddTransaction([FromBody] Transaction newTransaction)
+        {
+            _context.Transactions.Add(newTransaction);
+            _context.SaveChanges();
+
+            var createdTransaction = _context.Transactions
+                .Where(item => item.TransactionId == newTransaction.TransactionId)
+                    .Select(item => new
+                    {
+                        item.TransactionId,
+                        item.BookId,
+                        item.BuyerId,
+                        item.SellerId,
+                        item.TransactionDate,
+                        item.TotalAmount,
+                        item.DeliveryCharge,
+                        item.PlatformFee
+                    })
+                    .FirstOrDefault();
+
+            return CreatedAtAction("Get", new { id = newTransaction.TransactionId }, createdTransaction);
+        }
+    }
+}
