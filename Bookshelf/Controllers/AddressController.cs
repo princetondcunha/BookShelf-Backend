588672422b678e29dcab77d1ce75c30465eb50ed
A00472830 <princeton.hilary.dcunha@smu.ca>
2023-12-08 23:28:12 -0400

    Setup Address API

---
+++ Bookshelf/Controllers/AddressController.cs
@@ -0,0 +1,54 @@
+﻿using Bookshelf.Data;
+using Bookshelf.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookshelf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private readonly BookshelfDbContext _context;
+        public AddressController(BookshelfDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] Address newAddress)
+        {
+            try
+            {
+                _context.Addresses.Add(newAddress);
+                _context.SaveChanges();
+
+                var createdAddress = _context.Addresses
+                    .Where(item => item.AddressId == newAddress.AddressId)
+                    .Select(item => new
+                    {
+                        item.AddressId,
+                        item.UserId,
+                        item.Street,
+                        item.City,
+                        item.State,
+                        item.ZipCode,
+                        item.Country,
+                        item.Phone
+                    })
+                    .FirstOrDefault();
+
+                if (createdAddress == null)
+                {
+                    return NotFound();
+                }
+
+                return CreatedAtAction("Get", new { id = newAddress.AddressId}, createdAddress);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+            }
+        }
+    }
+}
